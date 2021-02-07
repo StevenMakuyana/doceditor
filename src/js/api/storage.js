7db21c509d6662ad6412c942c7738becf341ab7c
steven <stevenmakuyana94@gmail.com>
2021-02-07 02:36:19 -0800

    added storage initializer

--- src/js/api/storage.js
+++ src/js/api/storage.js
@@ -12,4 +12,12 @@ export default class Storage {
         this.storageName = 'doceditorV1';
         this.storage = null;
     }
+    init() {
+        if(localStorage.getItem(this.storageName)) {
+            this.storage = JSON.parse(localStorage.getItem(this.storageName));
+        } else {
+            localStorage.setItem(this.storageName, JSON.stringify(storageV1));
+            this.storage = JSON.parse(localStorage.getItem(this.storageName));
+        };
+    };
 };
