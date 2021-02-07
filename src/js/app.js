dd22e20e412235839020a864a38f9eddea147ac2
steven <stevenmakuyana94@gmail.com>
2021-02-07 02:10:01 -0800

    added mainDropdownEventHandler function

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
 import Visibility from './api/visibility.js';
+
+function mainDropdownEventHandler() {
+    const mainDropdown = document.getElementById('main-dropdown');
+
+    document.getElementById('main-dropdown-btn')
+        .addEventListener('click', () => {
+            Visibility(mainDropdown, 'show');
+            document.getElementById('main-dropdown-close-btn')
+                .addEventListener('click', () => {
+                    Visibility(mainDropdown, 'hide');
+                });
+        });
+};
+
+mainDropdownEventHandler();
