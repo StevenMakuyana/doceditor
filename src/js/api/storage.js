b1ace6b2b54f7aa3daec3b6c2fa0d7454f841a42
steven <stevenmakuyana94@gmail.com>
2021-02-07 02:25:06 -0800

    added storageV1 data

--- src/js/api/storage.js
+++ src/js/api/storage.js
@@ -1,3 +1,12 @@
+const storageV1 = {
+    config: {
+        account: {
+            hasAccount: false
+        }
+    },
+    document: []
+};
+
 export default class Storage {
     constructor() {
         this.storageName = 'doceditorV1';
