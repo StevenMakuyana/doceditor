31d261a78fcc2516a7d22669148fab405008c514
steven <stevenmakuyana94@gmail.com>
2021-02-07 01:20:15 -0800

    added switch case

--- src/js/api/visibility.js
+++ src/js/api/visibility.js
@@ -1,3 +1,24 @@
 export default function Visibility(element, action) {
-    //
+    switch (action) {
+        case 'show':
+            element.style.display = 'block';
+            let showFrames = 0;
+            function showELement() {
+                showFrames++
+                if (showFrames === 100) clearInterval(show);
+                else element.style.opacity = `${showFrames}%`;
+            };
+            const show = setInterval(showELement, 1);
+            break;
+        case 'hide':
+            let hideFrame = 100;
+            function hideElement() {
+                hideFrame--
+                if (hideFrame === 0) {
+                    clearInterval(hide);
+                    element.style.display = 'none';
+                } else element.style.opacity = `${hideFrame}%`;
+            };
+            const hide = setInterval(hideElement, 1);
+    };
 };
