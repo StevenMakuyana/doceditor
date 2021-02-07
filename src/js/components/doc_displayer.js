1ed6772181ea907c1012b4d28d08981b2dea458d
steven <stevenmakuyana94@gmail.com>
2021-02-07 07:48:15 -0800

    added documentsList

--- src/js/components/doc_displayer.js
+++ src/js/components/doc_displayer.js
@@ -1,1 +1,15 @@
-export default function docDisplayer() {};
+export default function docDisplayer(data = []) {
+    const documentsList = data.map(document => {
+        return `
+          <div>
+            <div class="name">
+              <i class="fa fa-file"></i>
+              <p>${document.title}</p>
+            <div>
+            <div class="date">
+              <p>${document.date}</p>
+            </div>
+          </div>
+        `
+    }).join("");
+};
